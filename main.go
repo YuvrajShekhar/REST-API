6eaac80d2dd9d8c28717866b6f5134b6d962cd75
Yuvraj Shekhar <syuvraj1999@gmail.com>
2025-03-20 16:40:31 +0100

    added GET and POST requests

--- main.go
+++ main.go
@@ -1,13 +1,39 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"example/restapi/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	router.Run() // listen and serve on 0.0.0.0:8080
+	gin.SetMode(gin.DebugMode)
+	server := gin.Default()
+	server.Use(gin.Logger())
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
+	server.Run(":8080")
+}
+
+func getEvents(context *gin.Context) {
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse the requested data", "error": err})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	event.Save()
+
+	context.JSON(http.StatusCreated, gin.H{"meesgage": "Event created!", "event": event})
+
 }
