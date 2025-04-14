fc816d322647d40fe17d41f8ba755229278a6daf
Yuvraj Shekhar <syuvraj1999@gmail.com>
2025-04-14 11:39:40 +0200

    added token verification and route protection

--- routes/event.go
+++ routes/event.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"example/restapi/models"
+	utils "example/restapi/utlis"
 	"net/http"
 	"strconv"
 
@@ -35,15 +36,28 @@ func getEvents(context *gin.Context) {
 }
 
 func createEvent(context *gin.Context) {
+	token := context.Request.Header.Get("Authorization")
+
+	if token == "" {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized"})
+		return
+	}
+
+	err := utils.VerifyToken(token)
+
+	if err != nil {
+		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized", "error": err})
+		return
+	}
+
 	var event models.Event
-	err := context.ShouldBindJSON(&event)
+	err = context.ShouldBindJSON(&event)
 
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "could not parse the requested data", "error": err})
 		return
 	}
 
-	event.ID = 1
 	event.UserID = 1
 	err = event.Save()
 	if err != nil {
